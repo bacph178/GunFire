006f74658008fe9167f06dec60c02ed0ab4383ea
Phung Hoang Bac <phung.hoang.bac@framgia.com>
2013-10-21 10:28:35 +0700

    Initial Commit

---
+++ GunFire/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  GunFireAppController.h
+//  GunFire
+//
+//  Created by macbook_016 on 2013/10/21.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ GunFire/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  GunFireAppController.h
+//  GunFire
+//
+//  Created by macbook_016 on 2013/10/21.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
